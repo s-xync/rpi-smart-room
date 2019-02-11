41f57d5b602dc0d3efa1e7c3cea42bf3d477a1a9
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-11 23:00:41 +0530

    adds server side code including sockets

---
+++ socket-switch/server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const socket = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const PORT = 4000;
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+const io = socket(server);
+
+io.on("connection", client => {
+  console.log(`Client connected with id ${client.id}`);
+});
