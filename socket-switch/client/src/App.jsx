032f3a14150d6904b5b0edb713ce01e2d830fbaa
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-02-13 22:05:38 +0530

    client side for temperature and humidity with axios

--- socket-switch/client/src/App.jsx
+++ socket-switch/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Switch from "react-switch";
+import axios from "axios";
 import "./App.css";
 import {
   statusEventListener,
@@ -9,17 +10,35 @@ import {
 
 class App extends Component {
   state = {
-    buttonStatus: false
+    buttonStatus: false,
+    temperature: 0,
+    humidity: 0
   };
+
   componentDidMount() {
     this.ejected = false;
     this.bindStatusEventListener();
+    this.getTempAndHumid();
   }
 
   componentWillUnmount() {
     this.ejected = true;
     closeSocket();
   }
+
+  getTempAndHumid = () => {
+    axios
+      .get(process.env.REACT_APP_SERVER_URL + "/tempandhumid")
+      .then(response => {
+        if (response.data.success) {
+          const { temperature, humidity } = response.data.message;
+          this.setState({ temperature, humidity });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   bindStatusEventListener = () => {
     statusEventListener((err, data) => {
@@ -37,13 +56,21 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        <p>
+        <p className="heading">
           Switch the light <strong>ON</strong> or <strong>OFF</strong>
         </p>
         <Switch
           onChange={this.handleChange}
           checked={this.state.buttonStatus}
         />
+        <br />
+        <br />
+        <p className="heading">Temperature and Humidity</p>
+        <p>{this.state.temperature}⁰C</p>
+        <p>{this.state.humidity}%RH</p>
+        <button className="refresh-button" onClick={this.getTempAndHumid}>
+          Refresh
+        </button>
       </div>
     );
   }
