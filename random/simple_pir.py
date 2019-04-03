e505c4e0ef4ad51e8186a23a93415f8a8586a7b8
Korthiwada <korthiwadarahul@gmail.com>
2019-04-03 19:20:13 +0530

    Adds simple code for PIR

---
+++ random/simple_pir.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(3, GPIO.OUT)         #LED output pin
+while True:
+i=GPIO.input(11)
+if i==0:                 #When output from motion sensor is LOW
+	print "No intruders",i
